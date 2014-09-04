dabc1a825a543d639201e20f2f1d40cb91928ffc
matt-manywho <matt.bates@manywho.com>
2014-09-04 09:39:25 +0100

    Replaced reference to 3rd party js dependency to point to the cdn

--- js/jquery.manywho.formBootStrap.js
+++ js/jquery.manywho.formBootStrap.js
@@ -3083,7 +3083,7 @@ permissions and limitations under the License.
                         "manywho_elements",
                     ],
                     external_plugins: {
-                        "moxiemanager": "/extensions/moxiemanager/plugin.js",
+                        "moxiemanager": "https://cdn.manywho.com/extensions/moxiemanager/plugin.js",
                         "nanospell": "https://flow.manywho.com/extensions/nanospell/plugin.js",
                     },
                     content_css: ["https://cdn.manywho.com/css/tinymce.css", "/css/tenant/" + ManyWhoSharedServices.getTenantId() + "/customstyles"],
