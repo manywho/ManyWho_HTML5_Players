d64ce0c11c3b08970c0b4a30c9105f7f70e7f0b6
matt-manywho <matt.bates@manywho.com>
2014-09-04 14:46:37 +0100

    Fixed players path in the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('default', function () {
     gulp.src('manywho.fileDataProxy.js')
         .pipe(gulp.dest('dist'));
 
-    gulp.src('players')
+    gulp.src('players/*.htm')
         .pipe(gulp.dest('dist_players'));
 
 });
