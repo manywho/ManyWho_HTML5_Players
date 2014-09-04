8f62ffdb76f0e0283061af8fa434ba00db864f79
matt-manywho <matt.bates@manywho.com>
2014-09-04 12:00:22 +0100

    updated gulpfile to copy delpoyable resources to the dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,35 @@
 var gulp = require('gulp');
+
+gulp.task('default', function () {
+   
+    gulp.src('build/**/*.*')
+        .pipe(gulp.dest('dist/build'));
+    
+    gulp.src('css/**/*.*')
+        .pipe(gulp.dest('dist/css'));
+
+    gulp.src('extensions/**/*.*')
+        .pipe(gulp.dest('dist/extensions'));
+
+    gulp.src('images/**/*.*')
+        .pipe(gulp.dest('dist/images'));
+
+    gulp.src('draw/**/*.*')
+        .pipe(gulp.dest('dist/draw'));
+
+    gulp.src('js/**/*.*')
+        .pipe(gulp.dest('dist/js'));
+
+    gulp.src('run/**/*.*')
+        .pipe(gulp.dest('dist/run'));
+
+    gulp.src('translate/**/*.*')
+        .pipe(gulp.dest('dist/translate'));
+
+    gulp.src('favicon.ico')
+        .pipe(gulp.dest('dist'));
+
+    gulp.src('manywho.fileDataProxy.js')
+        .pipe(gulp.dest('dist'));
+
+});
