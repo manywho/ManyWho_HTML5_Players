301bea8622318f5d1e0c124d3b2c0c9d72b57909
matt-manywho <matt.bates@manywho.com>
2014-09-22 16:16:26 +0100

    CORE-243 - Updated gulp file again so that it might actually replace the cdn url properly

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,36 @@ var replace = require('gulp-replace');
 var argv = require('yargs').argv;
 
 gulp.task('default', function () {
-   
+      
     gulp.src('build/**/*.*')
+		.pipe(replace('cdn.manywho.com', argv.cdn))
         .pipe(gulp.dest('dist/build'));
     
     gulp.src('css/**/*.*')
+		.pipe(replace('cdn.manywho.com', argv.cdn))
         .pipe(gulp.dest('dist/css'));
 
     gulp.src('extensions/**/*.*')
+		.pipe(replace('cdn.manywho.com', argv.cdn))
         .pipe(gulp.dest('dist/extensions'));
 
     gulp.src('images/**/*.*')
         .pipe(gulp.dest('dist/images'));
 
     gulp.src('draw/**/*.*')
+		.pipe(replace('cdn.manywho.com', argv.cdn))
         .pipe(gulp.dest('dist/draw'));
 
     gulp.src('js/**/*.*')
+		.pipe(replace('cdn.manywho.com', argv.cdn))
         .pipe(gulp.dest('dist/js'));
 
     gulp.src('run/**/*.*')
+		.pipe(replace('cdn.manywho.com', argv.cdn))
         .pipe(gulp.dest('dist/run'));
 
     gulp.src('translate/**/*.*')
+		.pipe(replace('cdn.manywho.com', argv.cdn))
         .pipe(gulp.dest('dist/translate'));
 
     gulp.src('favicon.ico')
@@ -33,25 +40,9 @@ gulp.task('default', function () {
 
     gulp.src('manywho.fileDataProxy.js')
         .pipe(gulp.dest('dist'));
-
-    if (argv.env == "staging") {
-
-        console.log("Setting CDN to staging");
-
-        gulp.src(['dist/**/*.css', 'dist/**/*.js', 'dist/**/*.html'])
-            .pipe(replace('cdn.manywho.com', 'cdn.staging.manywho.com'))
-            .pipe(gulp.dest('dist'));
-
-        gulp.src('players/*.htm')
-            .pipe(replace('cdn.manywho.com', 'cdn.staging.manywho.com'))
-            .pipe(gulp.dest('dist_players'));
-
-    }
-	else {
-	
-	    gulp.src('players/*.htm')
+		
+	gulp.src('players/*.htm')
+		.pipe(replace('cdn.manywho.com', argv.cdn))
         .pipe(gulp.dest('dist_players'));
-	
-	}
 
 });
